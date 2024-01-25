9ef2f362180ebc164f0f8b0b19448441b17038fb
stalien <stalien@yandex.ru>
2024-01-25 21:34:45 +0300

    get image and save it to file

--- src/main/java/BotApplication.java
+++ src/main/java/BotApplication.java
@@ -1,9 +1,11 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.hc.client5.http.classic.methods.HttpGet;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Properties;
 import java.util.Scanner;
@@ -14,14 +16,24 @@ public class BotApplication {
         var localProperties = new Properties();
         localProperties.load(new FileInputStream("src/main/resources/local.properties"));
 
-        var url = "https://api.nasa.gov/planetary/apod?api_key=" + localProperties.getProperty("API_KEY");
+        var url = "https://api.nasa.gov/planetary/apod?api_key=" +
+                localProperties.getProperty("API_KEY") +
+                "&date=2024-01-24";
 
         CloseableHttpClient client = HttpClients.createDefault();
 
         CloseableHttpResponse response = client.execute(new HttpGet(url));
 
-        Scanner scanner = new Scanner(response.getEntity().getContent());
+//        Scanner scanner = new Scanner(response.getEntity().getContent());
+//        System.out.println(scanner.nextLine());
 
-        System.out.println(scanner.nextLine());
+        ObjectMapper mapper = new ObjectMapper();
+        NasaAnswer answer = mapper.readValue(response.getEntity().getContent(), NasaAnswer.class);
+
+        CloseableHttpResponse image = client.execute(new HttpGet(answer.url));
+        String filename = answer.url.split("/")[answer.url.split("/").length - 1];
+        FileOutputStream fileOutputStream = new FileOutputStream("src/main/resources/img/" + filename);
+        fileOutputStream.write(image.getEntity().getContent().readAllBytes());
+
     }
 }
